d5abd8457e425cc4fece397efc2539ae3e9237fa
Sam Lu <sam.lu@robotsandpencils.com>
2020-06-03 14:42:51 -0600

    Switch assignment of property path to use double-quotes (#1)
    * Change input from property-path to property_path

--- index.test.js
+++ index.test.js
@@ -3,8 +3,8 @@ const cp = require('child_process');
 const path = require('path');
 
 test('integration test using httpbin.org', () => {
+    process.env["INPUT_PROPERTY_PATH"] = "slideshow.author";
     process.env['INPUT_URL'] = "https://httpbin.org/json";
-    process.env["INPUT_PROPERTY-PATH"] = 'slideshow.author';
 
     const ip = path.join(__dirname, 'index.js');
     let result = cp.execSync(`node ${ip}`, { env: process.env }).toString();
